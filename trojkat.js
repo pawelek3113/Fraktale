08230ad1117ec334db0d738126aaa36c2743be1a
pawelek3113 <pawel.kamil.komendera@gmail.com>
2023-03-04 20:32:01 +0100

    triangle

--- trojkat.js
+++ trojkat.js
@@ -7,15 +7,6 @@ function losowyKolor(){
     return color(random(255), random(255), random(255));
 }
 
-function drawTriangle(p1, p2, p3){
-    fill(losowyKolor());
-    triangle(p1.x, p1.y, p2.x, p2.y, p3.x, p3.y);
-}
-
-function srodkowyPunkt(p1, p2){
-    return createVector((p1.x + p2.x)/2, (p1.y + p2.y)/2); 
-}
-
 class Triangle{
     constructor(p1, p2, p3){
         this.p1 = p1;
@@ -27,7 +18,16 @@ class Triangle{
     }
 }
 
-var level = 3;
+function drawTriangle(p1, p2, p3){
+    fill(losowyKolor());
+    triangle(p1.x, p1.y, p2.x, p2.y, p3.x, p3.y);
+}
+
+function srodkowyPunkt(p1, p2){
+    return createVector((p1.x + p2.x)/2, (p1.y + p2.y)/2); 
+}
+
+var level = 4;
 
 function rysujTrojkatSierpinskiego(tr, n){
     if(n == level){
@@ -36,14 +36,14 @@ function rysujTrojkatSierpinskiego(tr, n){
     let m1 = srodkowyPunkt(tr.p1, tr.p2);
     let m2 = srodkowyPunkt(tr.p2, tr.p3);
     let m3 = srodkowyPunkt(tr.p3, tr.p1);
-
+    
     let t0 = new Triangle(m1, m2, m3);
     t0.draw();
-
+    
     let t1 = new Triangle(tr.p1, m1, m3);
     let t2 = new Triangle(tr.p2, m1, m2);
     let t3 = new Triangle(tr.p3, m2, m3);
-
+    
     rysujTrojkatSierpinskiego(t1, n+1);
     rysujTrojkatSierpinskiego(t2, n+1);
     rysujTrojkatSierpinskiego(t3, n+1);
@@ -54,10 +54,10 @@ function draw(){
     let p1 = createVector(600/2, 0);
     let p2 = createVector(0, 400);
     let p3 = createVector(600, 400);
-
+    
     let t1 = new Triangle(p1, p2, p3);
     t1.draw();
-
+    
     rysujTrojkatSierpinskiego(t1, 0);
     noLoop();
 }
